0de7c269d0d973372a3973e6ec2958f1ccd2dd2b
Maanav Khaitan <maanav@berkeley.edu>
2023-08-19 14:08:51 -0700

    Add randao submitter script

--- randao-submitter/index.ts
+++ randao-submitter/index.ts
@@ -1,7 +1,6 @@
 // Setup: npm install alchemy-sdk
-import { Alchemy, Network, AlchemyEventType } from "alchemy-sdk";
+import { Alchemy, BlockTag, Network } from "alchemy-sdk";
 
-// Optional config object, but defaults to demo api-key and eth-mainnet.
 const settings = {
   apiKey: process.env.ALCHEMY_API_KEY, // Replace with your Alchemy API Key.
   network: Network.ETH_GOERLI, // Replace with your network.
@@ -9,7 +8,45 @@ const settings = {
 
 const alchemy = new Alchemy(settings);
 
-// Subscription for new blocks on Eth Mainnet.
-alchemy.ws.on("block", (blockNumber) =>
-  console.log("The latest block number is", blockNumber)
+// This is the "randomnessRequested" event topic we want to watch.
+const randomnessRequestedTopic = "";
+// This is the contract we want to watch.
+const contractAddress = "";
+
+// Create the log options object.
+const randomnessRequestedEvents = {
+    address: contractAddress,
+    topics: [randomnessRequestedTopic],
+  };
+
+let blocksRequested: number[] = [];
+  
+const recordRandomnessRequested = (txn: any) => {
+    // TODO: Get the block number from the randomness request.
+    let blockNumber = 420;
+
+    // If we haven't already seen this block number, add it to the list.
+    if (!blocksRequested.includes(blockNumber)) {
+      blocksRequested.push(blockNumber);
+      console.log(`Randomness requested in block ${blockNumber}`);
+    }
+};
+
+// Listen for randomnessRequested events.
+alchemy.ws.on(randomnessRequestedEvents, recordRandomnessRequested);
+
+// Subscription for new blocks.
+alchemy.ws.on("block", async (blockNumber) => {
+    console.log("The latest block number is", blockNumber);
+    if (blocksRequested.includes(blockNumber)) {
+        let number: BlockTag = blockNumber;
+        let response = await alchemy.core.getBlock(number)
+
+        //Logging the RANDAO
+        console.log(response.difficulty);
+
+        // TODO: Submit a transaction onchain to provide the RANDAO to the contract
+        console.log("Fulfilled randomness for block", blockNumber);
+    }
+}
 );
